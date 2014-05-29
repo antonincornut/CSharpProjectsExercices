49aa4416a0956cfb5c5292fa7ff5282d6fa2c6c2
Antonin Cornut <antonin.cornut@gmail.com>
2014-05-29 17:29:27 +0200

    parse Commande Line OK

--- Nurl/Nurl/Processing.cs
+++ Nurl/Nurl/Processing.cs
@@ -9,12 +9,36 @@ namespace Nurl
 {
 	public class Processing
 	{
+
+
+		public String argsFirst { get; set; }
+		public String url { get; set; } 
+		public String pathFile { get; set; } 
+		public String nTime { get; set; } 
+		public String average { get; set; } 
+
 		public Processing ()
 		{
 		}
 
 		public Processing (string[] args)
-		{
+		{	
+	
+			string dir = null;
+			new OptionSet {
+				{ "url=|URL=", Url => url = Url },
+				{ "save=|SAVE=", Save => pathFile = Save },
+				{ "times=|TIMES=", Time => nTime = Time },
+				{ "avg|AVG", Average => average = Average },
+			}.Parse (args);
+
+			Console.Write(url);
+			Console.Write(pathFile);
+			Console.Write(nTime);
+			Console.Write(average);
+
+			if (dir == null)
+				throw new InvalidOperationException ("Missing required option -o=DIR");
 
 		}
 	}
